8c4ae50e725fdb8cf9c281deaef95e3b2bdc4949
beshoy <beshoynady993@gmail.com>
2024-05-09 23:58:24 +0300

    Adding new test case for view selected products name inside cart page

--- src/main/java/Pages/P07_ViewCartPage.java
+++ src/main/java/Pages/P07_ViewCartPage.java
@@ -1,8 +1,61 @@
 package Pages;
 
+import Utilities.LogsUtils;
+import Utilities.Utility;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class P07_ViewCartPage {
-    public P07_ViewCartPage(WebDriver driver) {
+    private final WebDriver driver;
+    private static final By selectedProducts = By.xpath("//td[@class=\"cart_description\"]//a[@href]");
+    private static List<WebElement> SelectedProducts;
+    private String name;
+
+   public P07_ViewCartPage(WebDriver driver)
+   {
+       this.driver = driver;
+   }
+
+    public P07_ViewCartPage getSelectedProductsName() {
+
+
+
+    SelectedProducts = driver.findElements(selectedProducts);
+    for (int i = 1; i <= SelectedProducts.size(); i++) {
+
+        By productsName = By.xpath("(//td[@class=\"cart_description\"]//a[@href])[" + i + "]");
+        LogsUtils.info("selected products name in cart page: " + Utility.getText(driver, productsName));
+        name= Utility.getText(driver, productsName);
     }
+
+
+
+        return this;
+    }
+
+    public String getNameProductFromCart()
+    {
+        try {
+
+           return getSelectedProductsName().name;
+
+        }catch (Exception e){
+
+            LogsUtils.error(e.getMessage());
+            return "";
+        }
+
+    }
+
+
+    public boolean verifyOnSelectedName()
+    {
+
+        return  getNameProductFromCart().equals(new P05_ProductsPage(driver).getProductsName());
+    }
+
+
 }
